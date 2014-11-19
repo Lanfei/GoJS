8d6c0fdb6afab3f12bf9d7141e07372094b94f87
Lanfei <CooLanfei@163.com>
2014-11-19 19:56:43 +0800

    Update documents

--- examples/assets/js/println.js
+++ examples/assets/js/println.js
@@ -1,9 +1,7 @@
 define(function() {
 
-	document.body.innerHTML += '<div id="content"></div>';
-
 	return function(msg) {
-		document.getElementById('content').innerHTML += '<p>' + (msg || '') + '</p>';
+		document.body.innerHTML += '<p>' + (msg || '') + '</p>';
 	};
 
 });
