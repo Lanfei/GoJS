e02aec9c2f9f1b6058c714de1c01280ceb9aad76
Lanfei <CooLanfei@163.com>
2015-03-06 18:55:15 +0800

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,38 @@
 module.exports = function(grunt) {
 
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json')
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			default: {
+				options: {
+					// sourceMap: true,
+					banner: '/*! <%= pkg.name %> <%= pkg.version %> | <%= pkg.description %> */\n'
+				},
+				files: {
+					'dist/go.js': 'src/go.js'
+				}
+			},
+			plugins: {
+				files: {
+					'dist/go-css.js': 'src/go-css.js',
+					'dist/go-json.js': 'src/go-json.js'
+				}
+			}
+		},
+		replace: {
+			default: {
+				overwrite: true,
+				src: ['dist/go.js'],
+				replacements: [{
+					from: '@VERSION',
+					to: '<%= pkg.version %>'
+				}]
+			}
+		}
 	});
 
 	grunt.loadNpmTasks('grunt-text-replace');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 
-	grunt.registerTask('default', function() {
-
-		grunt.initConfig({
-			uglify: {
-				default: {
-					files: {
-						'dist/go.js': 'src/go.js',
-						'dist/go-css.js': 'src/go-css.js',
-						'dist/go-json.js': 'src/go-json.js'
-					}
-				}
-			},
-			replace: {
-				default: {
-					src: ['dist/go.js'],
-					dest: ['dist/go.js'],
-					replacements: [{
-						from: '@VERSION',
-						to: grunt.config.get('pkg').version
-					}]
-				}
-			}
-		});
-
-		grunt.task.run(['uglify', 'replace']);
-	});
+	grunt.registerTask('default', ['uglify', 'replace']);
 };
